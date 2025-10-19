2f73af731f9da4de143ef9c0cb881293afaf11aa
Jasmine May Darman <Jelloismynick@gmail.com>
2025-10-19 13:44:03 -0400

    TheTeleportPuzzleOfAllTime
    Puzzle

--- Assets/LWLML_Project/Jasmine/Scripts/TriggerTag.cs
+++ Assets/LWLML_Project/Jasmine/Scripts/TriggerTag.cs
@@ -7,16 +7,18 @@ public class TriggerTag : MonoBehaviour
 [SerializeField] private string tagName; // Tag to detect
     [SerializeField] UnityEvent trigger; // Subscribes Methods via Inspector
 
-    public void OnCollisionEnter(Collision collision)
+    //on trigger enter with the specified tag, invoke the event
+    public void OnTriggerEnter(Collider other)  
     {
         //debug log
-        Debug.Log("Collision detected with object: " + collision.gameObject.name);
-        if (collision.gameObject.CompareTag(tagName))
+        Debug.Log("Trigger detected with object: " + other.gameObject.name);
+        if (other.gameObject.CompareTag(tagName))
         {
             trigger.Invoke();   // Sends the signal
         }
        
-    }
+    }   
+ 
 
     public void TestMethod()
     {
